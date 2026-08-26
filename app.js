caa0bb17d403a123e5b0c3c5aaff0a6cf91b63c3
BGMI GIVER <akg85360@gmail.com>
2026-08-26 13:40:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,66 +3,151 @@ const no = document.querySelector("#no");
 const gif = document.querySelector("#gif");
 const text = document.querySelector("#text");
 const vid = document.querySelector("video");
+
 let count = 2;
 
+
+// All GIFs
 const gifs = [
-  "../resources/cat-heart.gif",
-  "../resources/rusure.gif",
-  "../resources/3shocked-1.gif",
-  "../resources/4.crying.gif",
-  "../resources/5.crying.gif",
-  "../resources/idc.gif"
+    "cat-heart.gif",
+    "rusure.gif",
+    "3shocked-1.gif",
+    "4.crying.gif",
+    "5.crying.gif",
+    "idc.gif"
 ];
 
-// to load the gifs faster
+
+// Preload GIFs
 gifs.forEach(gifSrc => {
-  const img = new Image();
-  img.src = gifSrc;
+    const img = new Image();
+    img.src = gifSrc;
 });
 
-// changes for when NO button is clicked
+
+// =========================
+// NO BUTTON
+// =========================
+
 no.addEventListener("click", () => {
-  if (count == 2) {
-    gif.src = "../resources/rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
-    text.innerHTML = "You meant to press YES right?🤨";
-    yes.style.height = "65%";
-    yes.style.width = "60%";
-    no.style.width = "30%";
-    count++;
-  } else if (count == 3) {
-    gif.src = "../resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
-    text.innerHTML = "Your hand must have slipped right?🥹";
-    yes.style.height = "70%";
-    yes.style.width = "70%";
-    no.style.width = "20%";
-    count++;
-  } else if (count == 4) {
-    gif.src = "../resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
-    text.innerHTML = "I'm gonna cry😭";
-    yes.style.height = "80%";
-    yes.style.width = "80%";
-    no.style.fontSize = "4vh";
-    no.style.width = "10%";
-    count++;
-  } else if (count == 5) {
-    gif.src = "../resources/5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
-    text.innerHTML = "Pretty Please🥺😘";
+
+    // First NO
+    if (count === 2) {
+
+        gif.src = "rusure.gif";
+
+        text.innerHTML =
+            "You meant to press YES right? 🤨";
+
+        yes.style.height = "65%";
+        yes.style.width = "60%";
+
+        no.style.width = "30%";
+
+        count++;
+    }
+
+
+    // Second NO
+    else if (count === 3) {
+
+        gif.src = "3shocked-1.gif";
+
+        text.innerHTML =
+            "Your hand must have slipped right? 🥹";
+
+        yes.style.height = "70%";
+        yes.style.width = "70%";
+
+        no.style.width = "20%";
+
+        count++;
+    }
+
+
+    // Third NO
+    else if (count === 4) {
+
+        gif.src = "4.crying.gif";
+
+        text.innerHTML =
+            "I'm gonna cry 😭";
+
+        yes.style.height = "80%";
+        yes.style.width = "80%";
+
+        no.style.fontSize = "4vh";
+        no.style.width = "10%";
+
+        count++;
+    }
+
+
+    // Fourth NO
+    else if (count === 5) {
+
+        gif.src = "5.crying.gif";
+
+        text.innerHTML =
+            "Pretty Please 🥺😘";
+
+        yes.style.height = "90%";
+        yes.style.width = "96%";
+
+        /*
+            IMPORTANT:
+
+            NO does NOT disappear.
+
+            If Mansi presses NO again,
+            absolutely nothing happens.
+        */
+
+        count++;
+    }
+
+
+    // Fifth NO and onwards
+    else {
+
+        // Do absolutely nothing 😂
+
+    }
+
+});
+
+
+// =========================
+// YES BUTTON
+// =========================
+
+yes.addEventListener("click", () => {
+
+    // Show heart video
+    vid.style.display = "block";
+
+    // Final GIF
+    gif.src = "idc.gif";
+
+    // Final message
+    text.innerHTML =
+        "Knew it babe 😘❤️";
+
+    // Change button
+    yes.innerHTML =
+        '<a href="https://www.instagram.com/anish_kumar16_2009/" target="_blank">Message me ❤️</a>';
+
+    // Make YES big
     yes.style.height = "90%";
     yes.style.width = "96%";
-    no.style.display = "none";
-  }
-});
 
-// changes for when YES button is clicked
-yes.addEventListener("click", () => {
-  vid.style.display = "block";
-  gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
-  text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href="https://www.instagram.com/anish_kumar16_2009/">Message me</a>';// inside the " " put your social media profile link
-  yes.style.height = "90%";
-  yes.style.width = "96%";
-  no.style.display = "none";
-  setTimeout(() => {
-    vid.style.display = "none";
-  }, 9000);
+    // NO stays hidden after YES
+    no.style.display = "none";
+
+
+    // Hide video after 9 seconds
+    setTimeout(() => {
+        vid.style.display = "none";
+    }, 9000);
+
 });
